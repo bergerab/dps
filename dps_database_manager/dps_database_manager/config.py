2fd2f6b22eebb9d7e2eb1760fcb54469b82af7f9
Adam Berger <bergerab@uwm.edu>
2021-03-19 09:12:21 -0500

    Linux Docker

--- dps_database_manager/dps_database_manager/config.py
+++ dps_database_manager/dps_database_manager/config.py
@@ -6,5 +6,5 @@ URL        = os.getenv('TSDB_URL', 'localhost')
 PORT       = os.getenv('TSDB_PORT', 5432)
 DATABASE   = os.getenv('TSDB_DATABASE', 'postgres')
 DEBUG      = bool(os.getenv('DBM_DEBUG'))
-CONNECTION = f'postgres://{USERNAME}:{PASSWORD}@{URL}:{PORT}/{DATABASE}' \
-    if PASSWORD else f'postgres://{USERNAME}@{URL}:{PORT}/{DATABASE}'
+CONNECTION = f'postgresql://{USERNAME}:{PASSWORD}@{URL}:{PORT}/{DATABASE}' \
+    if PASSWORD else f'postgresql://{USERNAME}@{URL}:{PORT}/{DATABASE}'
--- dps_manager/dps_manager/wsgi.py
+++ dps_manager/dps_manager/wsgi.py
@@ -9,17 +9,22 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
-from dps_manager_api.models import Object
-from dps_manager_api.views import SystemAPI
+
+import django
+django.setup()
+
+# from dps_manager_api.models import Object
+# from dps_manager_api.views import SystemAPI
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dps_manager.settings')
 
 # If there are no systems, initialize the default objects
 # This is a bit hacky, should probably put some object into the
 # database when this is loaded, and check a version on that object.
-if not Object.objects.filter(kind=SystemAPI.kind).first():
-    call_command('loaddata', 'objects')
+# if not Object.objects.filter(kind=SystemAPI.kind).first():
+#     call_command('loaddata', 'objects')
 
 application = get_wsgi_application()
