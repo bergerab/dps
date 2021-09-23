506a8a7d3f959762a969f7ba52214f93250fca9b
Adam Berger <Adam.Berger@pkware.com>
2021-09-23 13:21:02 -0500

    Updated fixture file to add ID number in file name to allow for multiple fixtures. Updated WSGI script to automatically load all fixtures in every fixture directory. Updated units and descriptions for all electrical KPIs (except battery KPIs)

--- dps_manager/dps_manager/wsgi.py
+++ dps_manager/dps_manager/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+import pathlib
 
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
@@ -25,6 +25,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dps_manager.settings')
 # This is a bit hacky, should probably put some object into the
 # database when this is loaded, and check a version on that object.
 if not Object.objects.filter(kind=SystemAPI.kind).first():
-    call_command('loaddata', 'objects')
+    print('No systems were configured. Loading fixtures...')
+    cmd_args = list(pathlib.Path().glob('*/fixtures/*.json'))
+    call_command('loaddata', *cmd_args)
 
 application = get_wsgi_application()
